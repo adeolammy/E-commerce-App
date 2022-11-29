75500250286b4531410bc66d3748e15df477870e
adeolammy <adeolammy2020@gmail.com>
2022-11-29 16:20:20 +0100

    adding materail icons to the project

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,57 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Search } from "@material-ui/icons"
 
 const Container = styled.div`
 height: 60px;
-background-color: black; 
-`
+`;
+
 const Wrapper = styled.div`
-padding: 10px 20px;
- 
-`
+padding: 10px 20px; 
+display: flex;
+align-item: center;
+justify-content: space-between;
+`;
+
+const Left = styled.div `
+flex: 1;
+display:flex;
+align-item: center;
+`;
+
+const Language = styled.span`
+font-size:14px;
+cursor:pointer;
+` 
+const SearchContainer = styled.div`
+border: 0.5px solid lightgray;
+display: flex;
+align-item:center ;
+margin-left: 25px;
+padding:5px;
+`;
+
+const Input = styled.div`
+
+`;
+
+const Center = styled.div `flex: 1;`;
+const Right = styled.div `flex: 1;`;
+
 
 const NavBar = () => {
   return (
     <Container>
       <Wrapper> 
-          NavBar
+          <Left>
+          <Language> EN  </Language> 
+          <SearchContainer>
+            <Input/>
+            <Search/>
+          </SearchContainer>
+         </Left>
+          <Center> center </Center>
+          <Right> right </Right>
       </Wrapper>
     </Container>
   )
