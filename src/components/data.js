b78e7c6bcf5385437e194c1a1f19e6d130760338
adeolammy <adeolammy2020@gmail.com>
2022-11-30 17:56:53 +0100

    categories and categoriesItem components added

--- src/components/data.js
+++ src/components/data.js
@@ -8,7 +8,7 @@ export const sliderItems = [
     },
     {
         id: 2,
-        img: "https://images.pexels.com/photos/1589216/pexels-photo-1589216.jpeg?auto=compress&cs=tinysrgb&w=400",
+        img: "https://images.pexels.com/photos/1164674/pexels-photo-1164674.jpeg?auto=compress&cs=tinysrgb&w=600",
         title: 'WINTER SALES',
         desc: 'DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.',
         bg: "fcfled"
@@ -27,22 +27,29 @@ export const sliderItems = [
 export const categories = [
     {
         id: 1,
-        img: "https://images.pexels.com/photos/5886041/fashion-men-s-individuality-black-and-white-157675.jpeg?auto=compress&cs=tinysrgb&w=400",
+        img: "https://images.pexels.com/photos/1689731/pexels-photo-1689731.jpeg?auto=compress&cs=tinysrgb&w=400",
         title: 'SHIRT STYLE!',
         
     },
     {
         id: 2,
-        img: "https://images.pexels.com/photos/2983464/pexels-photo-1589216.jpeg?auto=compress&cs=tinysrgb&w=400",
-        title: 'LOUNGEWEAR LOVE',
+        img: "https://images.pexels.com/photos/3021263/pexels-photo-3021263.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title: 'OUTDOOR WEAR',
         
     },
     {
         id: 3,
-        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&w=400",
-        title: 'LIGHT JACKETS',
+        img: "https://images.pexels.com/photos/2531156/pexels-photo-2531156.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title: 'ARMLESS ',
         
     },
+    {
+        id: 4,
+        img: "https://images.pexels.com/photos/9717819/pexels-photo-9717819.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        title: 'CASUAL STYLE!',
+        
+    },
+  
 
 
 ]
