9e0c66290972b583835a6d5dfba1ec62dacfccbe
adeolammy <adeolammy2020@gmail.com>
2022-11-30 16:39:52 +0100

    Slider and data component added to the project

---
+++ src/components/data.js
@@ -0,0 +1,25 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/157675/fashion-men-s-individuality-black-and-white-157675.jpeg?auto=compress&cs=tinysrgb&w=400",
+        title: 'SUMMER SALES',
+        desc: 'DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.',
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/1589216/pexels-photo-1589216.jpeg?auto=compress&cs=tinysrgb&w=400",
+        title: 'WINTER SALES',
+        desc: 'DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.',
+        bg: "fcfled"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/1926769/pexels-photo-1926769.jpeg?auto=compress&cs=tinysrgb&w=400",
+        title: 'AUTUMN COLLECTIONS',
+        desc: 'DONT COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.',
+        bg: "fbf0f4"
+    },
+
+
+]
