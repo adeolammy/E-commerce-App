fcac7f25abfc7356e68693c5a622a3f3017574f8
adeolammy <adeolammy2020@gmail.com>
2022-12-02 14:28:49 +0100

    Adding the Register component

---
+++ src/Pages/Register.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Container = styled
+
+const Register = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default Register
